1461d0d90a9d29ef61788f5df2df6fe0911ad718
LukasdeSouza <lucasdesouzasilva112@gmail.com>
2023-02-28 14:12:59 -0300

    creating components

--- src/components/box/index.tsx
+++ src/components/box/index.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Typography } from '@mui/material'
+import SearchBar from '../searchbar'
+interface SearchBar {
+  cityName: string,
+  onChangeCity: React.ChangeEventHandler,
+  onChangeState: SelectChangeEvent<HTMLSelectElement>,
+  searchForWeather: () => void,
+  children: typeof React.Children
+}
 
-const WeatherBox = () => {
+const WeatherBox = ({ cityName, onChangeCity, onChangeState, searchForWeather, children }: SearchBar) => {
   return (
-    <div>WeatherBox</div>
+    <div>
+      <Typography variant='h6'>Pesquisar cidade e estado</Typography>
+      <SearchBar cityName={cityName}
+        onChangeCity={onChangeCity}
+        onChangeState={onChangeState}
+        searchForWeather={searchForWeather} />
+      {children}
+    </div>
   )
 }
 
---
+++ src/components/searchbar/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { brazilStates } from '@/utils/constants'
+import { Button, Input, MenuItem, Select, SelectChangeEvent, TextField } from '@mui/material'
+
+
+const SearchBar = ({ cityName, onChangeCity, onChangeState, searchForWeather }) => {
+  return (
+    <section>
+      <TextField type="text"
+        size='small'
+        label='Cidade'
+        value={cityName}
+        onChange={onChangeCity}
+      />
+      <Select
+        size='small'
+        label='Estados'
+        onChange={onChangeState}
+        sx={{ width: '180px' }}
+      >
+        {brazilStates.map((state) => (
+          <MenuItem
+            value={state.value}
+            key={state.key}
+          >
+            {state.label}
+          </MenuItem>
+        ))}
+      </Select>
+      <Button
+        variant='contained'
+        onClick={searchForWeather}>
+        Buscar
+      </Button>
+
+    </section>
+  )
+}
+
+export default SearchBar
